1f1bd508f1597d49d5ccf27e3aea6b128a67d2be
varn <vanrbu@gmail.com>
2019-11-02 16:58:27 +0800

    集成 文档工具

--- src/main/java/com/example/demo/web/UserController.java
+++ src/main/java/com/example/demo/web/UserController.java
@@ -1,27 +1,66 @@
 package com.example.demo.web;
 
 import com.example.demo.domain.User;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+
 import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
 
+@Api(tags = "用户管理")
 @RestController
 @RequestMapping(value = "/users")
 public class UserController {
     static Map<Long, User> users = Collections.synchronizedMap(new HashMap<Long, User>());
 
+    /**
+     * 获取用列表
+     *
+     * @return List
+     */
     @GetMapping("/")
+    @ApiOperation(value = "获取用户列表")
     public List<User> getUseList() {
         List<User> r = new ArrayList<User>(users.values());
         return r;
     }
 
     @GetMapping("/{id}")
+    @ApiOperation(value = "获取用户详细信息", notes = "根据 id 获取用户信息")
     public User getUser(@PathVariable Long id) {
         return users.get(id);
     }
 
+    @PutMapping("/{id}")
+    @ApiImplicitParam(paramType = "path", dataType = "Long", name = "id", value = "用户编号", required = true, example = "1")
+    @ApiOperation(value = "更新用户详细信息", notes = "根据url的id来指定更新对象，并根据传过来的user信息来更新用户详细信息")
+    public String putUser(@PathVariable Long id, @RequestBody User user) {
+        User u = users.get(id);
+        u.setName(user.getName());
+        u.setAge(user.getAge());
+        users.put(id, u);
+        return "success";
+    }
+
+    @DeleteMapping("/{id}")
+    @ApiOperation(value = "删除用户", notes = "根据url的id来指定删除对象")
+    public String deleteUser(@PathVariable Long id) {
+        users.remove(id);
+        return "success";
+    }
+
     @PostMapping("/")
+    @ApiImplicitParams({
+            @ApiImplicitParam(paramType = "path", dataType = "Long", name = "id", value = "编号", required = true, example =
+                    "1"),
+            @ApiImplicitParam(paramType = "path", dataType = "String", name = "name", value = "姓名", required = true,
+                    example = "小明")
+    })
+    @ApiOperation(value = "创建用户", notes = "根据 User 对象创建用户")
     public String postUser(@RequestBody User user) {
         users.put(user.getId(), user);
         return "{\"result\":\"success\"}";
